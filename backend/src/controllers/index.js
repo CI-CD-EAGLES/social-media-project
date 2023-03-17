12e7a91e0881cd5798a586e86ac6198b3ec0a770
shjr1 <shhxuingccf@gmail.com>
2023-03-17 10:56:18 -0700

    feat(controller): Added controller

--- backend/src/controllers/index.js
+++ backend/src/controllers/index.js
@@ -28,3 +28,88 @@ exports.getAllPost = async (req, res) => {
         })
     }
 }
+
+exports.getPostByUsername = async (req, res) => {
+    const postUserName = req.params.user_name;
+
+    try {
+        const post = await Posts.findOne({
+            where: { user_name: postUserName }
+        });
+
+        // CHECK IF POST ID IS VALID OR NOT
+        if(!post) {
+            res.status(400).json({
+                success: false,
+                message: `Post not found - Check post id!`
+            })
+        } else {
+            res.status(200).json({
+                post,
+                success: true,
+                message: `Post found succesfully!`
+            })
+        }
+    } catch (error) {
+        debug(error);
+        res.status(400).json({
+            success: false,
+            message: `Post not found - ERROR: ${error.message}`
+        })
+    }
+};
+
+exports.deletePostById = async (req, res) => {
+    const postId = req.params.id;
+
+    try {
+        const postToDelete = await Posts.findByPk(postId);
+        const deletedPost = await postToDelete.destroy();
+
+        // DELETE POST
+        res.status(200).json({
+            deletedPost,
+            success: true,
+            message: `Post deleted succesfully!`
+        });
+    } catch (error) {
+        debug('ERROR: ', error);
+        res.status(400).json({
+            success: false,
+            message: `Unable to delete post - ERROR: ${error.message}`
+        });
+    }
+};
+
+exports.updatePost = async (req, res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        res.status(400).json({
+            success: false,
+            error: errors.array()
+        });
+    } else {
+        const postId = req.params.id;
+        const updates = req.body;
+
+        try {
+            // FIND POST BY ID
+            const postToUpdate = await Posts.findByPk(postId);
+            // UPDATE POST WITH THE UPDATESS ENTERED BY USER
+            const updatedPost = await postToUpdate.update(updates);
+
+            res.status(200).json({
+                updatedPost,
+                success: true,
+                message: `Post updated succesfully!`
+            });
+        } catch (error) {
+            debug(error);
+            res.status(400).json({
+                success: false,
+                message: `Unable to update post - ERROR: ${error.message}`
+            });
+        }    
+    }
+};
