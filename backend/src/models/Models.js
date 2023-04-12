5a5596fddb88a54ecda69019ff7bb34080669559
Jonathan Jacka <jonathanjacka@gmail.com>
2023-04-12 13:33:10 -0500

    Removes unneeded fields from models and matches seed data to changes

--- backend/src/models/Models.js
+++ backend/src/models/Models.js
@@ -2,28 +2,18 @@ const sequelize = require('../db');
 const { Sequelize, DataTypes } = require('sequelize');
 
 const Posts = sequelize.define('post', {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
-    },
-    user_name: DataTypes.TEXT,
-    written_content: DataTypes.TEXT,
-    time_created: DataTypes.DATE,
-    num_of_likes: DataTypes.INTEGER,
+  written_content: DataTypes.TEXT,
+  num_of_likes: DataTypes.INTEGER,
 });
 
 const Users = sequelize.define('user', {
-    user_name: {
-        primaryKey: true,
-        type: Sequelize.STRING,
-    },
-    password: Sequelize.STRING,
-    profile_pic: Sequelize.STRING,
-    bio: DataTypes.TEXT,
+  user_name: Sequelize.STRING,
+  password: Sequelize.STRING,
+  profile_pic: Sequelize.STRING,
+  bio: DataTypes.TEXT,
 });
 
 Users.hasMany(Posts);
 Posts.belongsTo(Users);
 
-module.exports = {Posts, Users};
+module.exports = { Posts, Users };
